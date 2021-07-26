57b742b239d9ab099d8170c04612900c6acc98d2
YoannYWD <yoann.guidec@gmail.com>
2021-07-26 14:07:13 +0200

    ajout page create joueur

---
+++ create.php
@@ -0,0 +1,47 @@
+<?php
+require "./db.php";
+if(isset($_POST["nom"]) && isset($_POST["numero"]) && isset($_POST["position"])) {
+    $nom = $_POST["nom"];
+    $numero = $_POST["numero"];
+    $position = $_POST["position"];
+    $sql = "INSERT INTO joueur (nom, numero, position) VALUES (:nom, :numero, :position)";
+    $statement = $connection->prepare($sql);
+    if($statement->execute([":nom" => $nom, ":numero" => $numero, ":position" => $position])) {
+        print "Joueur ajouté";
+    }
+}
+?>
+
+
+<?php
+include "./head.php";
+?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-6 offset-3 my-5">
+            <h1 class="text-center mb-5">Ajouter un joueur</h1>
+            
+            <form method="post">
+              <div class="mb-3">
+                <label>Nom</label>
+                <input type="text" name="nom" class="form-control">
+              </div>
+              <div class="mb-3">
+                <label>Numéro</label>
+                <input type="number" name="numero" class="form-control">
+              </div>
+              <div class="mb-3">
+                <label>Position</label>
+                <input type="text" name="position" class="form-control">
+              </div>
+              <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+        </div>
+    </div>
+</div>
+
+
+<?php
+include "./footer.php";
+?>
